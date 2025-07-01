b4f5af93ad5adacfe01756360703999ad440a2f5
Pablo de la Peña <p@shedcollective.org>
2025-07-01 10:51:51 +0100

    chore: Remove BaseMiddle

--- src/Controller/Base.php
+++ src/Controller/Base.php
@@ -12,38 +12,6 @@
 
 namespace Nails\Survey\Controller;
 
-use Nails\Common\Exception\NailsException;
-use Nails\Factory;
-
-// --------------------------------------------------------------------------
-
-/**
- * Allow the app to add functionality, if needed
- * Negative conditional helps with static analysis
- */
-if (!class_exists('\App\Survey\Controller\Base')) {
-    abstract class BaseMiddle extends \App\Controller\Base
-    {
-    }
-} else {
-    abstract class BaseMiddle extends \App\Survey\Controller\Base
-    {
-        public function __construct()
-        {
-            if (!classExtends(static::class, \App\Controller\Base::class)) {
-                throw new NailsException(sprintf(
-                    'Class %s must extend %s',
-                    parent::class,
-                    \App\Controller\Base::class
-                ));
-            }
-            parent::__construct();
-        }
-    }
-}
-
-// --------------------------------------------------------------------------
-
-abstract class Base extends BaseMiddle
+abstract class Base extends \Nails\Common\Controller\Base
 {
 }
